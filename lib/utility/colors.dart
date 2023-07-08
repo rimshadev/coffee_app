3608f2e564edc4af4d662b5307278c9ae18d0a68
nomanakram1 <mr.noman143@gmail.com>
2023-07-08 17:27:24 +0500

    init

---
+++ lib/utility/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kblackClr = Colors.black;
+const kwhiteClr = Colors.white;
+const kgreenClr = Colors.green;
+const kredClr = Colors.red;
+const ktextgreyClr = Color(0xff8C8C8E);
+const klightgreyClr = Color(0xffDFDEE0);
+const kblueClr = Colors.blue;
---
+++ lib/view/main_view/main_view.dart
@@ -0,0 +1,75 @@
+import 'package:coffee_app/utility/colors.dart';
+import 'package:coffee_app/view/home_view/home_view.dart';
+import 'package:flutter/material.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeView(),
+    Text(
+      'Order',
+      style: optionStyle,
+    ),
+    Text(
+      'Rewards',
+      style: optionStyle,
+    ),
+    Text(
+      "Profile",
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: kblackClr,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(_selectedIndex == 0 ? Icons.home : Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(_selectedIndex == 1
+                ? Icons.shopping_bag
+                : Icons.shopping_bag_outlined),
+            label: 'Order',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Rewards',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+                _selectedIndex == 3 ? Icons.person_2 : Icons.person_2_outlined),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: kwhiteClr,
+        unselectedItemColor: ktextgreyClr,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
